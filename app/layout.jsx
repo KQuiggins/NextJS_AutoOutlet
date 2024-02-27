af6070ffae2e175051d66ca5d8647b4dad0bb4ac
KQuiggins <kashfamilyvalues@gmail.com>
2024-02-27 15:33:59 -0500

    Add new page and layout components

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'AutoOutlet | Car parts you can trust',
+  description: 'AutoOutlet is a car parts store that offers a wide range of car parts and accessories for all makes and models. We offer the best quality car parts at the best prices.',
+  keywords: 'car parts, car accessories, auto parts, car parts store, car parts online, car parts and accessories, car parts shop, car parts near me, car parts warehouse',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
