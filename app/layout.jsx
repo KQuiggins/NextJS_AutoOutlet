563924336bc4bb06d86809547e219d8cc78144d7
KQuiggins <kashfamilyvalues@gmail.com>
2024-03-15 11:42:43 -0400

    Update dependencies and add footer component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: 'AutoOutlet | Car parts you can trust',
@@ -13,6 +14,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,35 @@
-
+import Image from "next/image";
+import Link from "next/link";
+import logo from '@/assets/images/logo.png'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
 
-export default Footer
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="bg-gray-200 py-4 mt-auto">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
+        <div className="mb-4 md:mb-0">
+          <Image src={logo} alt="Logo" className="h-8 w-auto" />
+        </div>
+        <div className="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0">
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/parts">Parts</Link>
+            </li>
+            <li>
+              <Link href="/terms">Terms of Service</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-sm text-gray-500 mt-2 md:mt-0">
+            &copy; {currentYear} PropertyPulse. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
