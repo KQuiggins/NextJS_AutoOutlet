1c5650fbb8275923593efbc8f9b28023d06c77d7
KQuiggins <kashfamilyvalues@gmail.com>
2024-03-30 07:40:17 -0400

    Refactor parts page and add new parts

--- app/parts/page.jsx
+++ app/parts/page.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import parts from "@/parts.json"; // Assuming parts is an array of objects
-import ImageCard from "@/components/PartsCard"; // Correct import path assumed
+import React from "react";
+import { FaCar, FaMoneyBillWave, FaUser} from 'react-icons/fa';
+import Link from "next/link";
+import parts from "@/parts.json";
+import ImageCard from "@/components/PartsCard";
 
 const PartsPage = () => {
   return (
@@ -10,20 +12,31 @@ const PartsPage = () => {
           <div className="text-center p-4 bg-gray-100 rounded-lg">
             <h2 className="text-xl font-semibold">No parts available</h2>
             <p>
-              There are no parts available at the moment. Please check back later.
+              There are no parts available at the moment. Please check back
+              later.
             </p>
           </div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
             {parts.map((part, index) => (
-              <ImageCard key={index} imageUrl={part.imageUrl}>
-                <h2 className="text-lg font-bold">{part.part_name}</h2>
-                <p>{part.description}</p>
-                <p>Price: ${part.price}</p>
-                <p>Car: {part.car} {part.year}</p>
-                <p>Seller: {part.seller_name}</p>
-                
-              </ImageCard>
+              <Link key={part._id} href={`/parts/${part._id}`} passHref>
+  <div>
+    <ImageCard imageUrl={part.imageUrl}>
+      <h2 className="text-lg font-bold">{part.part_name}</h2>
+      <p>{part.description}</p>
+      <div className="flex items-center gap-2">
+        <FaMoneyBillWave /><span>${part.price}</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <FaCar /><span>{part.car} {part.year}</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <FaUser /><span>{part.seller_name}</span>
+      </div>
+    </ImageCard>
+  </div>
+</Link>
+
             ))}
           </div>
         )}
