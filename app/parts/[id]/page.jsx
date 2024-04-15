3d0381ba8469959b7dd8a7895c712129082426b9
KQuiggins <kashfamilyvalues@gmail.com>
2024-04-15 15:08:10 -0400

    Refactor parts page and HomeParts component to use fetchPartById utility function

--- app/parts/[id]/page.jsx
+++ app/parts/[id]/page.jsx
@@ -1,13 +1,105 @@
-'use client'
-import { useRouter } from "next/navigation";
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { fetchPartById } from "@/utils/request";
+import PartHeaderImage from "@/components/PartHeaderImage";
 
 const PartPage = () => {
-  const router = useRouter();
+  const { id } = useParams();
 
-  console.log(router);
+  const [part, setPart] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPart = async () => {
+      if (!id) return;
+      try {
+        const part = await fetchPartById(id);
+        setPart(part);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching part", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (part === null) {
+      fetchPart();
+    }
+  }, [id, part]);
+
+  if (!part && !loading) {
+    return (
+      <div>
+        <h1 className="text=center text-2xl font-bold mt-10">Part not found</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>PartPage</div>
-  )
-}
+    <>
+      {!loading && part && (
+        <>
+          <PartHeaderImage image={`/${part.imageUrl}`} />
+          <section>
+            <div className="container m-auto py-6 px-6">
+              <Link
+                href="/parts"
+                className="text-blue-500 hover:text-blue-600 flex items-center"
+              >
+                <i className="fas fa-arrow-left mr-2"></i> Back to all parts
+              </Link>
+            </div>
+          </section>
+          <section className="bg-blue-50">
+            <div className="container m-auto py-10 px-6">
+              <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                <div>
+                  <h1 className="text-3xl font-bold text-blue-500 my-6">
+                    {part.part_name}
+                  </h1>
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div>
+                      <h2 className="text-xl font-bold">Description</h2>
+                      <p>{part.description}</p>
+                    </div>
+                    <div>
+                      <h2 className="text-xl font-bold">Price</h2>
+                      <p>${part.price}</p>
+                    </div>
+                    <div>
+                      <h2 className="text-xl font-bold">Car</h2>
+                      <p>
+                        {part.car} {part.year}
+                      </p>
+                    </div>
+                    <div>
+                      <h2 className="text-xl font-bold">Seller</h2>
+                      <p>{part.seller_name}</p>
+                    </div>
+                  </div>
+                </div>
+                <aside className="space-y-4">
+                  <button
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+                  >
+                    <i className="fas fa-bookmark mr-2"></i> Bookmark Part
+                  </button>
+                  <button
+                    className="bg-black hover:bg-gray-800 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+                  >
+                    <i className="fas fa-share mr-2"></i> Share Part
+                  </button>
+                </aside>
+              </div>
+            </div>
+          </section>
+        </>
+      )}
+    </>
+  );
+};
 
-export default PartPage
+export default PartPage;
--- utils/request.js
+++ utils/request.js
@@ -18,4 +18,23 @@ async function fetchParts() {
   }
 }
 
-export { fetchParts };
+// Fetch a single part by id
+async function fetchPartById(id) {
+  try {
+    if (!apiDomain) {
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/parts/${id}`);
+
+    if (!res.ok) {
+      throw new Error("An error occurred while fetching the data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export { fetchParts, fetchPartById };
