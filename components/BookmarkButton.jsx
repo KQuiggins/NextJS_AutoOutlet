b780f7e11ba904ce84cfffbe13c7e826de9ced95
KQuiggins <kashfamilyvalues@gmail.com>
2024-09-07 09:00:23 -0400

    feat: Update SavedParts component to display saved parts with images and details

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -39,9 +39,9 @@ const BookmarkButton = ({ part }) => {
        return toast.success(res.message);
 
     })
-
-
   };
+
+  if (loading) return <div>Loading...</div>;
   return isBookmarked ? (
     <button className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center" onClick={handleClick}>
       <FaBookmark className="mr-2" />
